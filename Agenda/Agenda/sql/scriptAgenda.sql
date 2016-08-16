4610b0714f11295c23763c81dcdd2d1762d19b45
Andres Blas Pujadas <andiblas@gmail.com>
2016-08-16 18:51:37 -0300

    cambio de apostrofe por comilla simple

--- Agenda/Agenda/sql/scriptAgenda.sql
+++ Agenda/Agenda/sql/scriptAgenda.sql
@@ -5,24 +5,24 @@ CREATE TABLE `personas`
   `idPersona` int(11) NOT NULL AUTO_INCREMENT,
   `Nombre` varchar(45) NOT NULL,
   `Telefono` varchar(20) NOT NULL,
-  'email' varchar(40) NOT NULL,
-  'fechaNac' datetime NOT NULL,
-  'tipoContacto' int NOT NULL,
-  'domicilio' int NOT NULL,
-  'calle' varchar(100) NOT NULL,
-  'altura' int NOT NULL,
-  'piso' int,
-  'depto' int,
-  'localidad' int,
+  `email` varchar(40) NOT NULL,
+  `fechaNac` datetime NOT NULL,
+  `tipoContacto` int NOT NULL,
+  `domicilio` int NOT NULL,
+  `calle` varchar(100) NOT NULL,
+  `altura` int NOT NULL,
+  `piso` int,
+  `depto` int,
+  `localidad` int,
   PRIMARY KEY (`idPersona`)
 );
 CREATE TABLE `localidades`
 (
-  'id' int AUTO_INCREMENT,
-  'descripcion' varchar(40) NOT NULL
+  `id` int AUTO_INCREMENT,
+  `descripcion` varchar(40) NOT NULL
 );
 CREATE TABLE `tiposContacto`
 (
-  'id' int AUTO_INCREMENT,
-  'descripcion' varchar(40) NOT NULL
+  `id` int AUTO_INCREMENT,
+  `descripcion` varchar(40) NOT NULL
 );

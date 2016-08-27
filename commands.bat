4d06622c627b2da1543f2c4fee4340365b89562d
alefherrera <=>
2016-08-27 16:48:42 -0300

    instalacion de jre

--- commands.bat
+++ commands.bat
@@ -1,3 +1,4 @@
 msiexec /i mysql-5.5.51-win32 /passive
 "C:\Program Files (x86)\MySQL\MySQL Server 5.5\bin\MySQLInstanceConfig.exe" -i -q ServiceName=MySQL RootPassword=root ServerType=DEVELOPER DatabaseType=INNODB Port=3306 Charset=utf8
 "C:\Program Files (x86)\MySQL\MySQL Server 5.5\bin\mysql.exe" --user=root --password=root < scriptAgenda.sql
+jre-8u101-windows-i586.exe /s INSTALLDIR=C:\Java
